5b51dba0369eca09246726c2e0c34ef49024bc7c
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2012-03-10 17:19:17 +0100

    Corregido el HTML del código de ejemplo

--- fix-highlighting.py
+++ fix-highlighting.py
@@ -3,6 +3,7 @@
 import sys
 import re
 from subprocess import Popen, PIPE
+import HTMLParser
 
 file = sys.argv[1]
 lang_sig_re = re.compile(r'\s*<pre class="programlisting">LANGUAGE=(\S+) ')
@@ -10,23 +11,24 @@ end_lang_sig_re = re.compile(r'</pre>$')
 
 f = open(file)
 state = 'init'
-code = ""
+code_html = ""
 lang = ""
 for line in f:
     m = lang_sig_re.search(line)
     if m:
         state = 'programlisting'
         lang = m.group(1)
-        code = re.sub(lang_sig_re, '', line)
+        code_html = re.sub(lang_sig_re, '', line)
     elif state == 'programlisting':
-        code = code + line
+        code_html = code_html + line
 
     if state == 'programlisting' and end_lang_sig_re.search(line):
-        code = re.sub(end_lang_sig_re, '', code)
+        code_html = re.sub(end_lang_sig_re, '', code_html)
         line = ""
         state = 'init'
         p = Popen(["source-highlight", "-s", lang], stdin=PIPE, stdout=PIPE)
-        p.stdin.write(code)
+        code = HTMLParser.HTMLParser().unescape(unicode(code_html, "utf-8"))
+        p.stdin.write(code.encode("utf-8"))
         p.stdin.close()
         print p.stdout.read().replace('<pre', '<pre class="programlisting"')
 
