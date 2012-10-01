3cdf9e7b4c1bea5cd2b2a7c79e34184b2d89b487
Joaquin Caraballo <jcaraballo.git@gmail.com>
2012-10-01 01:16:59 +0100

    Set URL to revision in pdf metadata and fix script
    so it actually uses the arguments

--- Makefile
+++ Makefile
@@ -71,7 +71,7 @@ book.pdf: book.tex cover.pdf
 		TEXINPUTS=/usr/share/dblatex/latex/style//::/etc/asciidoc/dblatex:/usr/share/dblatex/latex// pdflatex book.tex && \
 		TEXINPUTS=/usr/share/dblatex/latex/style//::/etc/asciidoc/dblatex:/usr/share/dblatex/latex// pdflatex book.tex && \
 		pdftk C=../cover.pdf B=book.pdf cat C B3-end output book-with-cover.pdf keep_final_id && \
-		../set_pdf_metadata.bash <book-with-cover.pdf >../book.pdf 'El camino a un mejor programador' 'Varios Autores'
+		../set_pdf_metadata.bash <book-with-cover.pdf >../book.pdf 'El camino a un mejor programador' 'Varios Autores' $(BUILD_ID_URL)
 
 book.tex: $(SOURCE_FILES)
 	a2x -a lang=es $(XSLT_OPTS) -f tex book.asc
--- set_pdf_metadata.bash
+++ set_pdf_metadata.bash
@@ -2,16 +2,20 @@
 
 title=$1
 author=$2
+url=$3
 
-[[ -z "$2" ]] && echo 'usage title author <input.pdf >output.pdf' && exit -1
+[[ -z "$3" ]] && echo 'usage <title> <author> <url> <input.pdf >output.pdf' && exit -1
 
 rm -Rf tmp-set_pdf_metadata && mkdir tmp-set_pdf_metadata && cd tmp-set_pdf_metadata 
 
 cat >input.pdf
 
 pdftk input.pdf dump_data output report.txt
-echo -e 'InfoKey: Title\nInfoValue: El camino a un mejor programador' >>report.txt
-echo -e 'InfoKey: Author\nInfoValue: Varios Autores' >>report.txt
+
+echo -e "InfoKey: Title\nInfoValue: ${title}" >>report.txt
+echo -e "InfoKey: Author\nInfoValue: ${author}" >>report.txt
+echo -e "InfoKey: Url\nInfoValue: ${url}" >>report.txt
+
 pdftk input.pdf update_info report.txt output output.pdf
 
 cat <output.pdf
